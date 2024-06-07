f22000c63bb602fd426e8a15096d50c1b8fc7bf5
22e116019vs@stu.teikyo-u.ac.jp <ec2-user@ip-172-31-95-120.ec2.internal>
2024-06-07 12:30:42 +0900

    02からのファイルです

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
-    //
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLImit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+     use HasFactory;
+     use SoftDeletes;
+     
+     protected $fillable = ['title','body'];
+
+     public function getPaginateByLimit(int $limit_count = 10)
+     {
+         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+     }
 }
